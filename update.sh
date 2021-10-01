8b676d0789ee2a94d172d4ae5b03567d2024a7bb
rbSparky <rishb369@gmail.com>
2021-10-01 13:27:06 +0530

    as

--- update.sh
+++ update.sh
@@ -10,6 +10,7 @@ if [ $new_version == $current_version ]; then
 	echo "Up to date with $current_version"	
 else
 	echo "Update to $new_version"
+	echo "$new_version" > version.txt
 	gh auth login 
 	gh pr create --title "$new_version" --body ""	
 	#gh pr create --project "test"
